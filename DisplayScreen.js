635c0e03a77ab2a69180daf0deb472b31119f146
MateuszShift <mat_mich@op.pl>
2026-05-31 13:34:43 +0200

    initial commit

---
+++ DisplayScreen.js
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native"
+
+export default function DisplayScreen() {
+    return (
+        <View>
+            <Text>Display Screen</Text>
+        </View>
+    )
+}
